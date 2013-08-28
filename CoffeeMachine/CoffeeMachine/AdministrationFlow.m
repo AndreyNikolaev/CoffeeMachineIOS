d97d980618ca7f25aa7c697eebc62d597db782b4
GracielaM <gracielamitkova@gmail.com>
2013-08-28 05:58:22 -0300

    ViewController, AdministrationFlow - some changes

--- CoffeeMachine/CoffeeMachine/AdministrationFlow.m
+++ CoffeeMachine/CoffeeMachine/AdministrationFlow.m
@@ -32,7 +32,7 @@
 {
     [super viewDidLoad];
     self.title=@"Administrator report";
-    self.navigationBar.backBarButtonItem.title=@"Coffee Machine";
+//    self.navigationBar.backBarButtonItem.title=@"Coffee Machine";
     self.tableView.tableHeaderView=[[UIView alloc]init];
     // Do any additional setup after loading the view from its nib.
     
@@ -51,11 +51,12 @@
         [super didReceiveMemoryWarning];
         // Dispose of any resources that can be recreated.
     }
-- (IBAction)switchBack:(id)sender {
-    ViewController *vc = [[ViewController alloc]initWithNibName:@"ViewController"bundle:nil ];
-    [[self navigationController ]setNavigationBarHidden:NO animated:YES];
-    [self.navigationController pushViewController:vc animated:YES];
-}
+
+//- (IBAction)switchBack:(id)sender {
+//    ViewController *vc = [[ViewController alloc]initWithNibName:@"ViewController"bundle:nil ];
+//    [[self navigationController ]setNavigationBarHidden:NO animated:YES];
+//    [self.navigationController pushViewController:vc animated:YES];
+//}
 
 
 - (NSInteger)numberOfSectionsInTableView :(UITableView *)tableView {
